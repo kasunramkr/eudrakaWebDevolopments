5d28a478cec86f2cc751be020abfaa49a52f7d43
Milinda Rajapakshe <milinda@codegen.net>
2020-03-26 09:11:59 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+function submitClick() {
+  alert('Submit : ' + $('#name').val());
+}
+
+function hideImg() {
+  $('#back_img').hide();
+}
+
+function showImg() {
+  $('#back_img').show();
+}
+
+function toggleImg() {
+  $('#back_img').toggle();
+}
+
+function fadeOutImg() {
+  $('#back_img').fadeOut('slow');
+}
+
+function fadeInImg() {
+  $('#back_img').fadeIn('slow');
+}
